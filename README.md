c5d043e5dc07095f9c37ca188f6e60426a9850c2
rysh <rysh.cact@gmail.com>
2017-01-06 08:22:41 +0900

    Multiple files, bundled together

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # hello-webpack2
+ここを参考に試してる  
+https://blog.madewithenvy.com/getting-started-with-webpack-2-ed2b86c68783#.fo4fjrtax  
 
 history  
 yarn add --dev webpack@2.1.0-beta.25 webpack-dev-server@2.1.0-beta.10
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const webpack = require("webpack");
 module.exports = {
   context: __dirname + "/src",
   entry: {
-    app: "./app.js",
+    // app: "./app.js",
+    app: ['./app.js', './events.js', './vendor.js'],
   },
   output: {
     path: __dirname + "/dist",
