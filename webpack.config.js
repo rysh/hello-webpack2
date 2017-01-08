0435f4914c25f5332be32c0d3a80f6ac410e4a70
rysh <rysh.cact@gmail.com>
2017-01-08 21:55:24 +0900

    fix dist path

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, './src'),
+    path: path.resolve(__dirname, './dist'),
     publicPath: '/assets',                          // New
   },
   devServer: {
